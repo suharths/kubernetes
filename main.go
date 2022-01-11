78110f2aa1b500bf2d95e0ced69c8377a97d6441
hengki-s <hengki.suhartoyo@gmail.com>
2022-01-11 18:20:42 +0700

    Contoh API Golang

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+func homeLink(w http.ResponseWriter, r *http.Request) { 
+	fmt.Fprintf(w, "Selamat Datang "+os.Getenv("NAMA")+" di Sinau Docker!")
+
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
